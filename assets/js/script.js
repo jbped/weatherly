17de4462fe535ec6251f6e2fd87e84aa1cf46dd1
jbped <pedigojacob@gmail.com>
2021-06-01 19:05:58 -0600

    implemented fetch for searched items, bug not collecting UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,48 @@
 var defaultCity = "Salt Lake City"
 var apiUrl = "https://api.openweathermap.org/data/2.5/"
 var currentApi = "weather?"
-var forecastApi = "onecall?"
+var forecastApi = "forecast?"
 var forecastExclude = "&exclude=minutely,hourly"
 var apiKey = "&units=imperial&appid=8740010d5ce12cafca0e2a2a6e2bcf85"
+
+// Current Weather DOM Selectors
+var locationTitle = $("#location-title");
+var currentTemp = $("#current-temp");
+var currentDate = $("#current-date");
+var currentWeather = $("#current-weather");
+var currentWind = $("#current-wind");
+var currentHumidity = $("#current-humidity");
+var currentUv = $("#current-uv");
+
+// Day 1 Forecast DOM Selectors
+var dayOneTitle = $("#day-one-title");
+var dayOneTemp = $("#day-one-temp-span");
+var dayOneWind = $("#day-one-wind-span");
+var dayOneHumidity = $("#day-one-humidity-span");
+
+// Day 2 Forecast DOM Selectors
+var dayTwoTitle = $("#day-two-title");
+var dayTwoTemp = $("#day-two-temp-span");
+var dayTwoWind = $("#day-two-wind-span");
+var dayTwoHumidity = $("#day-two-humidity-span");
+
+// Day 3 Forecast DOM Selectors
+var dayThreeTitle = $("#day-three-title");
+var dayThreeTemp = $("#day-three-temp-span");
+var dayThreeWind = $("#day-three-wind-span");
+var dayThreeHumidity = $("#day-three-humidity-span");
+
+// Day 4 Forecast DOM Selectors
+var dayFourTitle = $("#day-four-title");
+var dayFourTemp = $("#day-four-temp-span");
+var dayFourWind = $("#day-four-wind-span");
+var dayFourHumidity = $("#day-four-humidity-span");
+
+// Day 5 Forecast DOM Selectors
+var dayFiveTitle = $("#day-five-title");
+var dayFiveTemp = $("#day-five-temp-span");
+var dayFiveWind = $("#day-five-wind-span");
+var dayFiveHumidity = $("#day-five-humidity-span");
 
 // CURRENT LOCATION -------------------------------------------- START
 // On page load get current location or display default city
@@ -25,14 +64,35 @@ function success(pos) {
 
     $("#location-title").text("Current Location")
 
-    fetch(
-        apiUrl + forecastApi + "lat=" + lat + "&lon=" + lon + forecastExclude + apiKey
+    fetch (
+        apiUrl + currentApi + "lat=" + lat + "&lon=" + lon + apiKey
     )
-    .then(function(response){
-        return response.json();
+    .then (function(response){
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert("Unable to find location")
+        }
     })
-    .then(function(response){
+    .then (function(data){
+        renderCurrent(data)
 
+        return fetch (
+            apiUrl + forecastApi + "lat=" + lat + "&lon=" + lon + apiKey
+        ) 
+        .then (function(response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Unable to find location")
+            }
+        })
+        .then (function(data){
+            renderForecast(data)
+        })
+        .catch(function(error){
+            alert("Unable to connect to GitHub")
+        })
     })
   }
   
@@ -83,7 +143,8 @@ var locationLogic = function(search) {
         var city = null;
         var zip = search;
         console.log("Zip Code");
-        locationSearch(city, zip);
+        locationTitle.text(search);
+        getWeather(city, zip);
         locArrHandler(search);
     // If search is blank
     } else if (!search || search < 10000) {
@@ -97,7 +158,8 @@ var locationLogic = function(search) {
         }
         var city = editCity.join(" ");
         var zip = null;
-        locationSearch(city, zip);
+        locationTitle.text(city);
+        getWeather(city, zip);
         locArrHandler(city);
     }
 }
@@ -147,13 +209,140 @@ var renderLocArr = function() {
     }
 }
 
+var renderCurrent = function(response) {
+    var unixDate = response.dt;
+    var milliDate = unixDate * 1000;
+    var dateObj = new Date(milliDate);
+    var convertedDate = dateObj.toLocaleString();
+    console.log(convertedDate);
+    // Render Date
+    currentDate.text(convertedDate);
+    locationTitle.text(response.name);
+    currentTemp.text(response.main.temp);
+    currentWeather.text(response.weather[0].main);
+    currentWind.text(response.wind.speed + " mph")
+    currentHumidity.text(response.main.humidity + "%");
+    // currentUv.text(response.)
 
-var locationSearch = function(city, zip) {
-    console.log("City: ", city,"ZIP: ", zip);
 }
 
-var currentWeather = function(response) {
-    console.log(response);
+var renderForecast = function(response) {
+
 }
+
+var getWeather = function(city,zip) {
+    if (city !== null) {
+        fetch (
+            apiUrl + currentApi + "q=" + city + apiKey
+        )
+        .then (function(response){
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Unable to find location")
+            }
+        })
+        .then (function(data){
+            renderCurrent(data)
+    
+            return fetch (
+                apiUrl + forecastApi + "q=" + city + apiKey
+            ) 
+            .then (function(response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert("Unable to find location")
+                }
+            })
+            .then (function(data){
+                renderForecast(data)
+            })
+            .catch(function(error){
+                alert("Unable to connect to GitHub")
+            })
+        })
+    } else if (zip !== null) {
+        fetch (
+            apiUrl + currentApi + "zip=" + zip + apiKey
+        )
+        .then (function(response){
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Unable to find location")
+            }
+        })
+        .then (function(data){
+            renderCurrent(data)
+    
+            return fetch (
+                apiUrl + forecastApi + "zip=" + zip + apiKey
+            ) 
+            .then (function(response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert("Unable to find location")
+                }
+            })
+            .then (function(data){
+                renderForecast(data)
+            })
+            .catch(function(error){
+                alert("Unable to connect to GitHub")
+            })
+        })
+    }
+}
+
+// var locationForecast = function(city, zip) {
+//     if (city === null) {
+//         fetch (
+//         apiUrl + forecastApi + "zip=" + zip + apiKey
+//         )
+//         .then(function(response){
+//             return response.json();
+//         })
+//         .then(function(response){
+//             renderForecast(response)
+//         })
+//     } else if (zip === null) {
+//         fetch (
+//             apiUrl + currentApi + "q=" + city + apiKey
+//             )
+//             .then(function(response){
+//                 return response.json();
+//             })
+//             .then(function(response){
+//                 renderForecast(response)
+//             })
+//     }    
+// }
+
+// var currentWeather = function(city, zip) {
+//     if (city === null) {
+//         fetch (
+//         apiUrl + currentApi + "zip=" + zip + apiKey
+//         )
+//         .then(function(response){
+//             return response.json();
+//         })
+//         .then(function(response){
+//             renderCurrent(response)
+//         })
+//     } else if (zip === null) {
+//         fetch (
+//             apiUrl + currentApi + "q=" + city + apiKey
+//             )
+//             .then(function(response){
+//                 return response.json();
+//             })
+//             .then(function(response){
+//                 renderCurrent(response)
+//             })
+//     }
+
+// }
 
 loadLocations();
